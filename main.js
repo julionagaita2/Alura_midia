803cb8d21cba13ba0fa343ec05a3447a309ed5a9
julionagaita2 <111884078+julionagaita2@users.noreply.github.com>
2023-09-26 09:45:43 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
-var elementosComClasses = document.getElementsByClassName("tecla tecla_psh tecla_tic tecla_tom tecla_toim tecla_splash tecla_puff tecla_tim tecla_pom");
-
-function tocar(){
-    let audio = [document.querySelector('#som_tecla_pom'), document.querySelector('#som_tecla_clap'), document.querySelector('#som_tecla_tim'),document.querySelector('#som_tecla_puff'), document.querySelector('#som_tecla_splash'), document.querySelector('#som_tecla_toim'), document.querySelector('#som_tecla_psh'), document.querySelector('#som_tecla_tic'), document.querySelector('#som_tecla_tom')]
-    audio[5].play()
+function tocar(i) {
+    const audioElements = document.querySelectorAll('audio');
+    if (i >= 0 && i < audioElements.length) {
+        audioElements[i].play();
+    }
 }
