a39ea5810c092db8fe17654202dee94aed4bea38
julionagaita2 <111884078+julionagaita2@users.noreply.github.com>
2023-11-23 11:00:47 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,25 @@
-function tocar(i) {
-    const audioElements = document.querySelectorAll('audio');
-    if (i >= 0 && i < audioElements.length) {
-        audioElements[i].play();
+function tocaSom (idElementoAudio) {
+    document.querySelector(idElementoAudio).play();
+}
+ 
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+//para
+for (let contador = 0; contador < listaDeTeclas.length; contador++) {
+
+    const tecla = listaDeTeclas[contador];
+    const instrumento = tecla.classList[1];
+    const idAudio = `#som_${instrumento}`; //template string
+
+    tecla.onclick = function () {
+        tocaSom(idAudio);
+    }
+
+    tecla.onkeydown = function (){
+        tecla.classList.add('ativa');
+    }
+
+    tecla.onkeyup = function(){
+        tecla.classList.remove('ativa');
     }
 }
